a30da770dad40965e87f5183abd76bc3a8c1fd53
Roy Tennenbaum <roytenen10@gmail.com>
2023-04-09 18:17:57 +0300

    nothing changed

--- frontend/src/containers/Pins.jsx
+++ frontend/src/containers/Pins.jsx
@@ -5,6 +5,7 @@ import { CreatePin, Feed, Navbar, PinDetail, Search } from "../components";
 
 const Pins = ({ user }) => {
   const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
