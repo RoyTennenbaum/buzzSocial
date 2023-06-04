34291cb28e8fd00adefb8de0801fd523663208f1
Roy Tennenbaum <roytenen10@gmail.com>
2023-06-04 19:24:55 +0300

    remove useless import

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { MdDownloadForOffline } from "react-icons/md";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 import { client, urlFor } from "../client";
