97dee5c729ff6a26fd427b10d6315cbd1d55b7ef
Roy Tennenbaum <roytenen10@gmail.com>
2023-06-06 01:48:53 +0300

    Optimized most images on the website

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -5,11 +5,11 @@ export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: true, // set to `true` to fetch from edge cache
-  apiVersion: "2023-04-12", // use current date (YYYY-MM-DD) to target the latest API version!
+  apiVersion: "2023-06-06", // use current date (YYYY-MM-DD) to target the latest API version!
   token: process.env.REACT_APP_SANITY_TOKEN,
   ignoreBrowserTokenWarning: true,
 });
 
 const builder = ImageUrlBuilder(client);
 
-export const urlFor = (source) => builder.image(source);
+export const urlFor = (source) => builder.image(source).format("webp");
